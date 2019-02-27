320194ca659269807256bd7eb09d3541109a3bd7
Yuchen Qie <841821505@qq.com>
2019-02-26 19:24:00 -0800

    finish

---
+++ kernel.cu
@@ -0,0 +1,16 @@
+/******************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ******************************************************************************/
+
+#include <stdio.h>
+    
+
+__global__ void VecAdd(const float *A, const float *B, float* C, int n) {
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    if(i < n) 
+        C[i] = A[i] + B[i];
+}
